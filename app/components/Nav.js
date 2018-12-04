7442f8c4fdf17b293e3c80a879bc882cd6600e70
cubsfan46350 <cubsfan.46350@msn.com>
2018-12-04 02:29:43 -0600

    updated li and nav class active

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -5,7 +5,7 @@ function Nav () {
     return (
         <ul className='nav'>
             <li>
-                <NavLink activeClassName='active' to='/'>
+                <NavLink exact activeClassName='active' to='/'>
                     Home
                 </NavLink>
             </li>
